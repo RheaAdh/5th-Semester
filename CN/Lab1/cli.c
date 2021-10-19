8d09ddf191cab137f1408401e4fb7652ec651858
RheaAdh <rheadhikari@gmail.com>
2021-10-19 18:27:41 +0530

    lab1 update

---
+++ CN/Lab1/cli.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <strings.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include<netinet/in.h>
+#include<unistd.h>
+#include<stdlib.h>
+#define PORT 5000
+#define MAXLINE 1000
+
+
+int main()
+{
+	char buffer[100];
+	int sockfd, n,len;
+	struct sockaddr_in servaddr, cliaddr;
+	
+	char *message = "Hello Server";
+
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(PORT);
+	servaddr.sin_family = AF_INET;
+
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	sendto(sockfd, message, MAXLINE, 0, (struct sockaddr*)&servaddr, sizeof(servaddr));
+	len=sizeof(cliaddr);
+
+	n=recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr*)&cliaddr,&len );
+    buffer[n]='\0';
+	printf("Message from server is : %s \n",buffer);
+    getchar();
+
+	close(sockfd);
+}
---
+++ CN/Lab1/ser5.c
@@ -0,0 +1,49 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+int main()
+{
+    time_t rawtime;
+    struct tm *timeinfo;
+    char *reply;
+    int server_sockfd, client_sockfd;
+    int server_len, client_len;
+    struct sockaddr_in server_address;
+    struct sockaddr_in client_address;
+    int hour, mins, sec, pid;
+    server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_address.sin_port = 9734;
+    server_len = sizeof(server_address);
+    bind(server_sockfd, (struct sockaddr *)&server_address, server_len);
+    listen(server_sockfd, 5);
+    while (1)
+    {
+        char ch;
+        printf("server waiting\n");
+        client_len = sizeof(client_address);
+        client_sockfd = accept(server_sockfd, (struct sockaddr *)&client_address, &client_len);
+        char *ip_add = inet_ntoa(client_address.sin_addr);
+        int port = client_address.sin_port;
+        printf("IP:%s PORT:%d\n", ip_add, port);
+        time(&rawtime);
+        timeinfo = localtime(&rawtime);
+        reply = asctime(timeinfo);
+        printf("The current date/time is: %s", reply);
+        hour = timeinfo->tm_hour;
+        mins = timeinfo->tm_min;
+        sec = timeinfo->tm_sec;
+        pid = getpid();
+        write(client_sockfd, &hour, 1);
+        write(client_sockfd, &mins, 1);
+        write(client_sockfd, &sec, 1);
+        write(client_sockfd, &pid, 1);
+    }
+    return 0;
+}
