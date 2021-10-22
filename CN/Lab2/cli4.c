8be06ac3ef13146210a5ec5ff1ab8a0d223dd1e5
RheaAdh <rheadhikari@gmail.com>
2021-10-22 11:50:17 +0530

    files

--- CN/Lab2/cli4.c
+++ CN/Lab2/cli4.c
@@ -1,36 +1,48 @@
+#include <stdlib.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <time.h>
+
 int main()
 {
     int sockfd;
     int len;
     struct sockaddr_in address;
-    struct tm *timeinfo;
+    struct tm * timeinfo;
     int result;
     char *reply;
-    int hour, mins, sec, pid;
+    int hour,mins,sec,pid;
+
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = inet_addr("127.0.0.1");
     address.sin_port = 9734;
     len = sizeof(address);
+
     result = connect(sockfd, (struct sockaddr *)&address, len);
-    if (result == -1)
+
+    if(result == -1) 
     {
         perror("oops: client2");
         exit(1);
     }
+   
     printf(" Sending request to get the time\n");
-    read(sockfd, &hour, 1);
-    read(sockfd, &mins, 1);
-    read(sockfd, &sec, 1);
-    read(sockfd, &pid, 1);
+    
+    read(sockfd, &hour , 1);
+    read(sockfd, &mins , 1);
+    read(sockfd, &sec , 1);
+    read(sockfd, &pid , 1);
     printf("%d:%d:%d", hour, mins, sec);
-    printf(" The process id is: %d", pid);
+    printf(" The process id is: %d",pid);
+
     close(sockfd);
     exit(0);
+
     return 0;
 }
