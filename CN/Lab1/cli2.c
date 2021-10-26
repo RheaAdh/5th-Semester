40afa964b7a4b9558db665baa00d8385fe6c473a
RheaAdh <rheadhikari@gmail.com>
2021-10-26 10:38:22 +0530

    cleanup

--- CN/Lab1/cli2.c
+++ CN/Lab1/cli2.c
@@ -6,11 +6,13 @@
 #define MAX 80
 #define PORT 8080
 #define SA struct sockaddr
+
 void clifunc(int sockfd)
 {
 	char buff[MAX];
 	int n;
-	for (;;) {
+	for (;;)
+	{
 		bzero(buff, sizeof(buff));
 		printf("Enter the string : ");
 		n = 0;
@@ -20,7 +22,8 @@ void clifunc(int sockfd)
 		bzero(buff, sizeof(buff));
 		read(sockfd, buff, sizeof(buff));
 		printf("From Server : %s", buff);
-		if ((strncmp(buff, "exit", 4)) == 0) {
+		if ((strncmp(buff, "exit", 4)) == 0)
+		{
 			printf("Client Exit...\n");
 			break;
 		}
@@ -34,7 +37,8 @@ int main()
 
 	// socket create and verification
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd == -1) {
+	if (sockfd == -1)
+	{
 		printf("socket creation failed...\n");
 		exit(0);
 	}
@@ -48,7 +52,8 @@ int main()
 	servaddr.sin_port = htons(PORT);
 
 	// connect the client socket to server socket
-	if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) {
+	if (connect(sockfd, (SA *)&servaddr, sizeof(servaddr)) != 0)
+	{
 		printf("connection with the server failed...\n");
 		exit(0);
 	}
--- CN/Lab1/ser.c
+++ CN/Lab1/ser.c
@@ -3,35 +3,32 @@
 #include <sys/types.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-#include<netinet/in.h>
+#include <netinet/in.h>
 #define PORT 5000
 #define MAXLINE 1000
 
 int main()
 {
 	char buffer[100];
-	int servsockfd, len,n;
+	int servsockfd, len, n;
 	struct sockaddr_in servaddr, cliaddr;
+
 	bzero(&servaddr, sizeof(servaddr));
 
-
-	servsockfd = socket(AF_INET, SOCK_DGRAM, 0);		
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	servaddr.sin_port = htons(PORT);
 	servaddr.sin_family = AF_INET;
 
+	servsockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
-	bind(servsockfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
-	
-
-	len = sizeof(cliaddr);  
-	n = recvfrom(servsockfd, buffer, sizeof(buffer),0, (struct sockaddr*)&cliaddr,&len);
+	bind(servsockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+	len = sizeof(cliaddr);
+	n = recvfrom(servsockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)&cliaddr, &len);
 	buffer[n] = '\0';
 	puts(buffer);
 
-
-    sendto(servsockfd, buffer, n, 0, (struct sockaddr*)&cliaddr, sizeof(cliaddr));
-    getchar();
+	sendto(servsockfd, buffer, n, 0, (struct sockaddr *)&cliaddr, sizeof(cliaddr));
+	getchar();
 
 	close(servsockfd);
 }
